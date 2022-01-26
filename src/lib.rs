040cc8d39736aa9952994e2c94f2458e963614f2
Francososa <franky.sosa@gmail.com>
2022-01-26 14:18:14 -0600

    Update config struct and run function to take into account case sensitive envvar

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -11,10 +13,13 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     
-        Ok(Self { query, filename })
+        Ok(Self { query, filename, case_sensitive })
     } 
 }
 
@@ -45,9 +50,16 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search_case_sensitive(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search_case_sensitive(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
+
     Ok(())
 }
 

35952fe4f54e4dfdf393b89a6d741d47bd0c623b
Francososa <franky.sosa@gmail.com>
2022-01-26 11:46:54 -0600

    Implement search function

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,14 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line.trim())
+        }
+    }
+    result
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
